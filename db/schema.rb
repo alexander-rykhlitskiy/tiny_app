8dfe157cb94804eb0a90b19151b29612e67c06ca
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2022-08-26 18:40:04 +0200

    Add posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_08_26_163931) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id_id"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id_id"], name: "index_posts_on_user_id_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
